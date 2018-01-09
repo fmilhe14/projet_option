01e56e313c73128a0ac5443ef8c43b1f4e97b48b
fmilhe14 <francoismilhem@hotmail.fr>
2018-01-09 15:30:23 +0100

    Update

---
+++ java/src/Main.java
@@ -0,0 +1,11 @@
+import components.Component;
+import org.chocosolver.solver.Solver;
+
+public class Main {
+
+
+    public static void main(String[] args){
+
+
+    }
+}
--- java/src/components/Service.java
+++ java/src/components/Service.java
@@ -3,6 +3,7 @@ package components;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import org.chocosolver.solver.variables.IntVar;
 
 import java.util.List;
 
@@ -15,13 +16,16 @@ public class Service {
     private List<Component> unfixedComponents;
     private double[][] latencies;
     private double[][] bandwidths;
+    private IntVar[][][] paths;
 
     public Service(List<Component> fixedComponents, List<Component> unfixedComponents, double[][] latencies,
-                   double[][] bandwidths){
+                   double[][] bandwidths, IntVar[][][] paths){
 
         this.fixedComponents = fixedComponents;
         this.unfixedComponents = unfixedComponents;
         this.latencies = latencies;
         this.bandwidths = bandwidths;
-    }
+        this.paths = paths;
+
+        }
 }
